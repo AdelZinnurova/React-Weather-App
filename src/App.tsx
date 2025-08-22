30eb6254fed568a9a02b331b12d8cc27ebbecaca
AdelZinnurova <ad187174@bk.ru>
2025-08-22 14:46:29 +0300

    add error and loading

--- src/App.tsx
+++ src/App.tsx
@@ -1,42 +1,149 @@
 import './App.css'
 import {useEffect, useState} from "react";
 
+type WeatherApiResponse = {
+    "coord": {
+        "lon": number,
+        "lat": number
+    },
+    "weather": [
+        {
+            "id": number,
+            "main": string,
+            "description": string,
+            "icon": string
+        }
+    ],
+    "base": string,
+    "main": {
+        "temp": number,
+        "feels_like": number,
+        "temp_min": number,
+        "temp_max": number,
+        "pressure": number,
+        "humidity": number,
+        "sea_level": number,
+        "grnd_level": number
+    },
+    "visibility": number,
+    "wind": {
+        "speed": number,
+        "deg": number
+    },
+    "clouds": {
+        "all": number
+    },
+    "dt": number,
+    "sys": {
+        "type": number,
+        "id": number,
+        "country": string,
+        "sunrise": number,
+        "sunset": number
+    },
+    "timezone": number,
+    "id": number,
+    "name": string,
+    "cod": number
+}
+
+type ForecastApiResponse = {
+    cod: string;
+    message: number;
+    cnt: number;
+    list: ForecastItem[];
+    city: {
+        id: number;
+        name: string;
+        coord: { lat: number; lon: number };
+        country: string;
+        population: number;
+        timezone: number;
+        sunrise: number;
+        sunset: number;
+    };
+};
+
+type ForecastItem = {
+    dt: number;
+    main: {
+        temp: number; feels_like: number; temp_min: number; temp_max: number;
+        pressure: number; sea_level: number; grnd_level: number; humidity: number; temp_kf: number;
+    };
+    weather: { id: number; main: string; description: string; icon: string }[];
+    clouds: { all: number };
+    wind: { speed: number; deg: number; gust: number };
+    visibility: number;
+    pop: number;
+    sys: { pod: string };
+    dt_txt: string;
+}
+
 function App() {
 
     const API_KEY = 'e2b8e81c26191504257bd05c2e05b7aa'
 
-    const [weatherData, setWeatherData] = useState(null);
-    const [city, setCity] = useState("london");
-    const [forecast, setForecast] = useState([]);
+    const [weatherData, setWeatherData] = useState<WeatherApiResponse | null>(null);
+    const [city, setCity] = useState<string>("london");
+    const [forecast, setForecast] = useState<ForecastItem[]>([]);
+    const [searchInput, setSearchInput] = useState("");
+    const [error, setError] = useState(null);
+    const [loading, setLoading] = useState(false)
+
+    const fetchWeatherData = async (cityName) => {
+        setCity(cityName);
+        try {
+            setLoading(true)
+            setError(null)
+            const url = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${API_KEY}&units=metric`;
+            const response = await fetch(url);
+            const data = await response.json();
+            setWeatherData(data);
+            console.log(data);
+
+            const foreCastresponse = await fetch(
+                `https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${API_KEY}&units=metric`
+            );
+            const forecastdata = await foreCastresponse.json();
+
+            const dailyForecast = forecastdata.list.filter(
+                (item, index) => index % 8 === 0
+            );
+
+            setForecast(dailyForecast);
+        } catch {
+            setError("Couldnt fetch data,please try again")
+        } finally {
+            setLoading(false)
+        }
+    };
 
     useEffect(() => {
-        const fetchWeatherData = async (cityName: string) => {
-            try {
-                const url = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${API_KEY}&units=metric`;
-                const response = await fetch(url);
-                const data = await response.json();
-                setWeatherData(data);
-                console.log(data);
+        fetchWeatherData(city);
+    }, [city]);
 
-                const foreCastresponse = await fetch(
-                    `https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${API_KEY}&units=metric`
-                );
-                const forecastdata = await foreCastresponse.json();
-                console.log(forecastdata);
-                const dailyForecast = forecastdata.list.filter(
-                    (item, index) => index % 8 === 0
-                );
-                setForecast(dailyForecast);
-            } catch (error) {
-                console.log(error.message);
-            }
-        }
+    function handleSearch(e) {
+        e.preventDefault();
+        fetchWeatherData(searchInput);
+    }
 
-        fetchWeatherData(city)
-    }, [city])
+    if (loading) return <div className="wrapper">Loading...</div>
 
     return (
         <div className="wrapper">
+            <form onSubmit={handleSearch} className="search-form">
+                <input
+                    type="text"
+                    value={searchInput}
+                    onChange={(e) => setSearchInput(e.target.value)}
+                    placeholder="Enter city name"
+                    className="search-input"
+                />
+                <button type="submit" className="search-button">
+                    Search
+                </button>
+            </form>
+            {error && <p className="error">{error}</p>}
 
             {weatherData && weatherData.main && weatherData.weather && (
                 <>
